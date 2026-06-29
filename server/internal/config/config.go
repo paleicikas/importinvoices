3a933835d3198dd32900295ed1daf17e5232b240
Arturas Paleicikas <paleicikas@gmail.com>
2026-06-29 11:12:27 +0300

    Bump version to v1.2.1

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -28,7 +28,7 @@ func findAvailablePort(host string, start, end int) string {
 	return net.JoinHostPort(host, strconv.Itoa(start))
 }
 
-var Version = "1.2.0"
+var Version = "1.2.1"
 
 type Config struct {
 	DataDir         string   `json:"data_dir"`
